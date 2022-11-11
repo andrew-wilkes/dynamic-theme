dc6eeedd2bb1c84a0c1d14ad66c0988cec9ba0a5
andrew-wilkes <github@urgentclick.com>
2022-11-11 16:00:56 +0900

    Add main scene

--- Main.gd
+++ Main.gd
@@ -1,2 +1,12 @@
 extends Control
 
+func _on_BackgroundColor_color_changed(color):
+	pass # Replace with function body.
+
+
+func _on_ForegroundColor_color_changed(color):
+	pass # Replace with function body.
+
+
+func _on_ButtonColor_color_changed(color):
+	pass # Replace with function body.
